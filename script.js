e0c65c9c23c6a29591ba8bf81ed093ed0571d5f3
saimommostafa <saimommostafa@gmail.com>
2020-07-23 21:32:52 +0600

    Styling done

---
+++ script.js
@@ -0,0 +1,5 @@
+const fname = document.getElementById('fname')
+const lname = document.getElementById('lname')
+const email = document.getElementById('email')
+const pass = document.getElementById('pass')
+const form = document.getElementById('form')
